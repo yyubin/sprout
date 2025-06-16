7a972cbe4f659e59a5bc26c1f9d30300364f1181
yyubin <hazing120@gmail.com>
2025-06-17 05:15:15 +0900

    feat: resolver 구현체 추가

--- src/main/java/sprout/mvc/argument/builtins/SessionResolver.java
+++ src/main/java/sprout/mvc/argument/builtins/SessionResolver.java
@@ -1,4 +1,28 @@
 package sprout.mvc.argument.builtins;
 
-public class SessionResolver {
+import app.util.Session;
+import sprout.beans.annotation.Component;
+import sprout.mvc.argument.ArgumentResolver;
+import sprout.mvc.http.HttpRequest;
+
+import java.lang.reflect.Parameter;
+import java.util.Map;
+
+@Component
+public class SessionResolver implements ArgumentResolver {
+    @Override
+    public boolean supports(Parameter parameter) {
+        return parameter.getType().equals(Session.class) ||
+                (parameter.getType().equals(String.class) && parameter.getName().equals("sessionId"));
+    }
+
+    @Override
+    public Object resolve(Parameter parameter, HttpRequest<Map<String, Object>> request) throws Exception {
+        if (parameter.getType().equals(Session.class)) {
+            return null;
+        } else if (parameter.getType().equals(String.class) && parameter.getName().equals("sessionId")) {
+            return request.getSessionId();
+        }
+        return null;
+    }
 }
