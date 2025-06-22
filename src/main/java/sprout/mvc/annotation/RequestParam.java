de20bac14ad815ca7300c1ffac217da5b28a797d
yyubin <hazing120@gmail.com>
2025-06-23 03:27:13 +0900

    fix: annotation target 수정

--- src/main/java/sprout/mvc/annotation/RequestParam.java
+++ src/main/java/sprout/mvc/annotation/RequestParam.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.TYPE_PARAMETER)
+@Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RequestParam {
     String value() default "";
